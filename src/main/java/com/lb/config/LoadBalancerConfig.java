511c936db028ee5e65736ebadc7e26ec049e2b66
mourian.kk <mourian.kk@zohocorp.com>
2026-09-10 14:11:29 +0530

    [Configurable Algorithm by chnaging in application.yml]

--- src/main/java/com/lb/config/LoadBalancerConfig.java
+++ src/main/java/com/lb/config/LoadBalancerConfig.java
@@ -8,9 +8,11 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
+@Configuration
 public class LoadBalancerConfig {
 
 
